0824b7eae28f8e0dab41ad17c009789f668dbfa5
Agus Permadi <agsperr@gmail.com>
2021-07-31 19:51:48 +0700

    Commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PI2-CI3-HP/';
+$config['base_url'] = 'http://localhost/pi2-ci3-tugasakhir/';
 
 /*
 |--------------------------------------------------------------------------
